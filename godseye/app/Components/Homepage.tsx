91f49b46b81e185ce65c554abedb407e91b85790
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2024-01-18 21:20:34 +0530

    updates

---
+++ godseye/app/Components/Homepage.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Homepage = () => {
+    return (
+        <>
+            <main className="">
+                
+            </main>
+        </>
+    )
+}
+
+export default Homepage;
